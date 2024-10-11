353949d5a579c67c3cc1655d6601314f80877e04
Effie Armony <harmony.effie@gmail.com>
2024-10-11 23:26:28 +0300

    basic render of books

---
+++ cmps/BookPreview.jsx
@@ -0,0 +1,12 @@
+
+// export function BookPreview({ book }) {
+
+//     const { vendor, speed } = book
+//     return (
+//         <article className="book-preview">
+//             <h2>Vendor: {vendor}</h2>
+//             <h4>Book Speed: {speed}</h4>
+//             <img src={`../assets/img/${vendor}.png`} alt="Book Image" />
+//         </article>
+//     )
+// }
