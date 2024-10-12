b6d500117baf812755f768a59edd8d4399e551c1
Effie Armony <harmony.effie@gmail.com>
2024-10-12 13:43:48 +0300

    Add book details page & back btn

--- pages/BookIndex.jsx
+++ pages/BookIndex.jsx
@@ -36,3 +36,40 @@ export function BookIndex() {
         </section>
     )
 }
+
+// import { BookFilter } from "../cmps/BookFilter.jsx"
+
+// const [selectedBookId, setSelectedBookId] = useState(null)
+// const [filterBy, setFilterBy] = useState(bookService.getDefaultFilter())
+
+// function onRemoveBook(bookId) {
+//     bookService.remove(bookId)
+//         .then(() => {
+//             setBooks(books =>
+//                 books.filter(book => book.id !== bookId)
+//             )
+//         })
+//         .catch(err => {
+//             console.log('Problems removing book:', err)
+//         })
+// }
+
+// function onSetFilter(filterByToEdit) {
+//     setFilterBy(prevFilter => ({...prevFilter, ...filterByToEdit}))
+// }
+
+// function onSelectBookId(bookId) {
+//     setSelectedBookId(bookId)
+// }
+
+// return (
+//     <section className="book-index">
+//         {!selectedBookId
+//             ? <React.Fragment>
+//                 <BookFilter onSetFilter={onSetFilter} filterBy={filterBy} />
+//                 <BookList onSelectBookId={onSelectBookId} onRemoveBook={onRemoveBook} books={books} />
+//             </React.Fragment>
+//             : <BookDetails onBack={() => setSelectedBookId(null)} bookId={selectedBookId} />
+//         }
+//     </section>
+// )
