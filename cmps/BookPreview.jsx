f2572e9ca5e45a48cf854269b6bcd35b1bd97d9b
Effie Armony <harmony.effie@gmail.com>
2024-10-12 18:05:02 +0300

    Add OnSale

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,11 +1,20 @@
-export function BookPreview({book}) {
+import { OnSale } from "../cmps/OnSale.jsx"
+
+export function BookPreview({ book }) {
+ 
+    let sale = ''
+    if (book.listPrice.isOnSale) {
+        sale = <OnSale></OnSale>
+    }
 
     return (
         <article className="book-preview">
             <h2>{book.title}</h2>
-            <img src={book.thumbnail} alt="Book Image" />
+            <section className="book-img">
+                <img src={book.thumbnail} alt="Book Image" />
+                {sale}
+            </section>
             <p>Price: {book.listPrice.amount}</p>
         </article>
-
     )
 }
---
+++ cmps/OnSale.jsx
@@ -0,0 +1,8 @@
+export function OnSale(){
+
+    return(
+        <section className="on-sale">
+            <h4>ON SALE!</h4>
+        </section>
+    )
+}
